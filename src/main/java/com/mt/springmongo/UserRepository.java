884dfa1a9426d20f209a35525b573616c09539b2
FortressTechnologiesInc <130385349+FortressTechnologiesInc@users.noreply.github.com>
2025-03-14 10:48:28 -0400

    Delete src/main/java/com/mt/springmongo/UserRepository.java

--- src/main/java/com/mt/springmongo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prospersllc.repository;
-
-import com.prospersllc.model.CompanyInfo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyInfoRepository extends MongoRepository<CompanyInfo, String> {
-}
